0835d6b379ed05be96368bdb43556a14759518b6
ericincloud <144301872+ericincloud@users.noreply.github.com>
2024-03-18 15:32:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
